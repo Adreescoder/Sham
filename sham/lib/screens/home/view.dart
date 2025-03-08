f604b17079ea3c41ffdd1faf11e6cc53f4e78c59
Adrees Coder <adreesking975@gmail.com>
2025-03-08 14:37:57 +0500

    code test

---
+++ sham/lib/screens/home/view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+
+import 'logic.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  final HomeLogic logic = Get.put(HomeLogic());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Lottie.network("https://lottie.host/2cd74df5-b7f7-4526-ac39-e31137080bbd/5oUtB0AzZF.json"),
+      ),
+    );
+  }
+}
+
+// you i you
+// you i you
+// you i you
